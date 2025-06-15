fd47d9a596669b445dfac6f4c3c967b0147fac24
Vaishnavi0401 <vaishu.daber@gmail.com>
2025-06-15 17:59:28 -0400

    update validated_order.py

--- validate_order.py
+++ validate_order.py
@@ -6,26 +6,6 @@ catalog_df = pd.read_csv("product_catalog.csv")
 def validate_response(requested_products, catalog_df):
 
     #Assuming we get the response in a list of dictionaries with the fields product_name, quantity, description, delivery_requirements 
-    requested_products = [
-    {
-        "product_name": "Desk TRÄNHOLM 19",
-        "description": "Not available",
-        "quantity": 8,
-        "delivery_requirements": "Deliver to 2-11-3 Meguro, Tokyo, Japan before June 18, 2025"
-    },
-    {
-        "product_name": "Desk NORDMARK 476",
-        "description": "Not available",
-        "quantity": 1,
-        "delivery_requirements": "Deliver to 2-11-3 Meguro, Tokyo, Japan before June 18, 2025"
-    },
-    {
-        "product_name": "Desk VIKTSTA 642",
-        "description": "Not available",
-        "quantity": 3,
-        "delivery_requirements": "Deliver to 2-11-3 Meguro, Tokyo, Japan before June 18, 2025"
-    }
-    ]
 
     validated = []
 
